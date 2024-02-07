a8776f549e07389747cd3f603dc9feed2115a838
Hugo Assoignon <97449839+HugoAssoignon@users.noreply.github.com>
2024-02-07 15:39:37 +0100

    Add files via upload
    last update deplacement centré du perso

--- code/game.py
+++ code/game.py
@@ -2,6 +2,8 @@ import pygame
 
 from screen import Screen
 from map import Map
+from entity import Entity
+from keylistener import KeyListener
 
 
 class Game:
@@ -9,8 +11,21 @@ class Game:
         self.running = True
         self.screen = Screen()
         self.map = Map(self.screen)
+        self.keylistener = KeyListener()
+        self.entity = Entity(self.keylistener)
+        self.map.add_player(self.entity)
 
     def run(self):
         while self.running:
+            self.handle_input()
             self.map.update()
             self.screen.update()
+
+    def handle_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                self.keylistener.add_key(event.key)
+            elif event.type == pygame.KEYUP:
+                self.keylistener.remove_key(event.key)
--- code/map.py
+++ code/map.py
@@ -13,12 +13,21 @@ class Map:
         self.group = None
 
         self.switch_map("map0")
+        self.player=None
 
     def switch_map(self, map_name: str):
         self.tmx_data = pytmx.load_pygame(f"../assets/map/{map_name}.tmx")
         map_data = pyscroll.data.TiledMapData(self.tmx_data)
         self.map_layer = pyscroll.BufferedRenderer(map_data, self.screen.get_size())
+        self.map_layer.zoom = 2
         self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer, default_layer=7)
 
+
+    def add_player(self, player):
+        self.group.add(player)
+        self.player = player
+
     def update(self):
+        self.group.update()
+        self.group.center(self.player.rect.center)
         self.group.draw(self.screen.display)
