eeddea386648f812e27002f90cafce3e7b57e3e7
Korentin <korentin.kolaczek@gmail.com>
2024-02-12 08:46:19 +0100

    mise en place de l'interface de test

--- pokemoche/code/sql.py
+++ pokemoche/code/sql.py
@@ -1,8 +1,10 @@
+import pathlib
 import sqlite3
+
 
 class SQL:
     def __init__(self):
-        self.connection = sqlite3.connect("")
+        self.connection = sqlite3.connect("../../assets/base.db")
         self.cursor = self.connection.cursor()
 
     def get_connection(self):
@@ -11,5 +13,17 @@ class SQL:
     def get_cursor(self):
         return self.cursor
 
-    def select(self,table: str, columns: list[str], where: str = None,order: str = None,limit: int =None) -> list[tuple]:
-        query: str = f"SELECT{'.'}
+    def select(self, table: str, columns: list[str], where: str = None, order: str = None, limit: int = None) -> list[
+        tuple]:
+        query: str = f"SELECT {'. '.join(columns)} FROM {table}"
+        if where:
+            query += f" WHERE {where}"
+        if order:
+            query += f" ORDER BY {order}"
+        if limit:
+            query += f" LIMIT {limit}"
+        print(query)
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+
