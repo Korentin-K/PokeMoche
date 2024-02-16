d85b365af391385c5a74f123545cade1a91995bd
Hugo Assoignon <97449839+HugoAssoignon@users.noreply.github.com>
2024-02-16 12:02:23 +0100

    Add files via upload
    maj file et correction source assets normalement !!!

---
+++ assets/map/tileset/bourg-en-vol.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="bourg-en-vol" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="bourg-en-vol.png" width="320" height="320"/>
+</tileset>
---
+++ assets/map/tileset/route101.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="route101" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="route101.png" width="320" height="320"/>
+</tileset>
