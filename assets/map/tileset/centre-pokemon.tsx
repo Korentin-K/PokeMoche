7c373c1aa6e04336ab955d44a0dd4d9a7deab6e1
Hugo Assoignon <97449839+HugoAssoignon@users.noreply.github.com>
2024-02-18 16:09:59 +0100

    Add files via upload
    ajout nouvelles maps et assets associés . Manque que le pokéshop & 1ère salle arène si je la fais...

---
+++ assets/map/tileset/centre-pokemon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="centre-pokemon" tilewidth="16" tileheight="16" tilecount="160" columns="16">
+ <image source="centre-pokemon.png" width="256" height="164"/>
+</tileset>
