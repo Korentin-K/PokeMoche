7fc4a6cc7e3bd95cf7e99a79492099e2f29876fd
Hugo Assoignon <97449839+HugoAssoignon@users.noreply.github.com>
2024-02-18 18:20:57 +0100

    Add files via upload
    toutes les maps

---
+++ assets/map/tileset/arene-clementi-ville.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="arene-clementi-ville" tilewidth="16" tileheight="16" tilecount="1073" columns="29">
+ <image source="arene-clementi-ville.png" width="472" height="600"/>
+</tileset>
