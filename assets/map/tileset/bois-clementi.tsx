a98a52ae938376c5fe09f7fcde77242c715e2399
Hugo Assoignon <97449839+HugoAssoignon@users.noreply.github.com>
2024-02-22 14:36:08 +0100

    Add files via upload
    maj des files avant de repull puis ssh

---
+++ assets/map/tileset/bois-clementi.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="bois-clementi" tilewidth="16" tileheight="16" tilecount="2112" columns="48">
+ <image source="bois-clementi.png" width="768" height="704"/>
+</tileset>
---
+++ assets/map/tileset/route116.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="route116" tilewidth="16" tileheight="16" tilecount="2000" columns="100">
+ <image source="route116.png" width="1600" height="320"/>
+</tileset>
