ceb918b8d18f25d345300ef1ee837c350c14ec71
Korentin <korentin.kolaczek@gmail.com>
2024-02-27 18:32:11 +0100

    delete pokemoche

--- pokemoche/assets/map/tileset/tileset_pokemon_sdk.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset_pokemon_sdk" tilewidth="16" tileheight="16" tilecount="4256" columns="8">
- <image source="../../../../../Downloads/tileset_pokemon_sdk.png" width="128" height="8512"/>
-</tileset>
