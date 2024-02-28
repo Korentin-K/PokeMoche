1971431001e3337adf288fd01e04016de9502f04
EdouardHoest <hoest.edouard@gmail.com>
2024-02-28 21:55:46 +0100

    feat(fix runAway): we were not able to run away and start another battle

--- assets/map/tileset/arene-merouville.tsx
+++ assets/map/tileset/arene-merouville.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="arene-merouville" tilewidth="16" tileheight="16" tilecount="220" columns="11">
- <image source="../../../../../PycharmProjects/Pokémoche/assets/map/tileset/arene-merouville.png" width="178" height="322"/>
+ <image source="arene-merouville.png" width="178" height="322"/>
 </tileset>
--- assets/map/tileset/route103.tsx
+++ assets/map/tileset/route103.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="route103" tilewidth="16" tileheight="16" tilecount="1600" columns="80">
- <image source="../../../../../PycharmProjects/Pokémoche/assets/map/tileset/route103.png" width="1280" height="320"/>
+ <image source="route103.png" width="1280" height="320"/>
 </tileset>
